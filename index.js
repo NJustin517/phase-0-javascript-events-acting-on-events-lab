f1f3e1d5c0230ed5bb0dd60281a2a08e94298aac
Justin Luciano <justinlluciano@gmail.com>
2021-12-19 17:12:16 -0800

    commit tree 2cf9a91f75da22b65a0035d92849a1b89873f01c

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+const dodger = document.querySelector("#dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+const moveDodgerLeft = () => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+const moveDodgerRight = () => {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers);
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`;
+  }
+};
+
+document.addEventListener("keydown", (pressedKey) => {
+  if (pressedKey.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (pressedKey.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
